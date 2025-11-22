2e5725999923ebdfd05e8c070406ca5eec0635d3
Nick Nguyen <khangnick14@gmail.com>
2025-11-23 00:04:10 +0700

    feat: init spring boot project

---
+++ src/main/java/com/trading/system/SystemApplication.java
@@ -0,0 +1,13 @@
+package com.trading.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemApplication.class, args);
+	}
+
+}
